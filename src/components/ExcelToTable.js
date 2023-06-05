ec8db32304a0647ee9f3d2ede956719664222e23
Tadeo <conversotadeo@gmail.com>
2023-06-05 17:06:31 -0300

    add and delete

--- src/components/ExcelToTable.js
+++ src/components/ExcelToTable.js
@@ -1,66 +1,91 @@
 import { useState } from "react";
 
-/* //TODO - 
-- El copy debe ser a partir de un campo que se despliega con un botón "insertar campos"
-- Limitar el texto de X e Y
-- Mapear los campos del table head
-- Borrar y editar a partir de acción con botones individuales para cada línea
- */
-
 const ExcelToTable = () => {
+  const newPoint = {
+    merchant: "",
+    address: "",
+    x: "",
+    y: "",
+    normalizedAddress: "",
+    marker: "",
+  };
   const [tableData, setTableData] = useState([]);
+  const [addPoint, setAddPoint] = useState(false);
+  const headers = Object.keys(newPoint);
 
-  // Parse text and populate the table with pasted data
   const populateTable = (data) => {
     const newData = data
-      .split("\n") // Split the data into rows
-      .map((row) => row.trim()) //Trim white spaces around each text
+      .split("\n")
+      .map((row) => row.trim())
       .filter((trimmedRow) => trimmedRow !== "")
       .map((trimmedRow) => trimmedRow.split("\t").map((cell) => cell.trim()));
 
-    console.log(newData);
-    setTableData(newData);
+    const newObjects = newData.map((row) =>
+      row.reduce((obj, cell, index) => {
+        obj[Object.keys(newPoint)[index]] = cell;
+        return obj;
+      }, {})
+    );
+
+    setTableData((prevData) => [...prevData, ...newObjects]);
   };
 
-  // Handle paste event on the table
   const handlePaste = (e) => {
-    // Get the clipboard data
     const clipboardData = e.clipboardData || window.clipboardData;
     const pastedData = clipboardData.getData("text");
-
-    // Prevent default paste behavior
     e.preventDefault();
-
-    // Populate the table with pasted data
     populateTable(pastedData);
+  };
+
+  const handleDelete = (index) => {
+    setTableData((prevData) => prevData.filter((_, i) => i !== index));
+  };
+
+  const handleAddPoint = () => {
+    setAddPoint((prevState) => !prevState);
   };
 
   return (
     <div>
-      <table style={{ border: "2px solid" }} onPaste={handlePaste}>
+      <table style={{ border: "2px solid", width: "100%" }} onPaste={handlePaste}>
         <thead>
           <tr>
-            <th>Merchant</th>
-            <th>Address</th>
-            <th>X</th>
-            <th>Y</th>
-            <th>NORMALIZED ADDRESS</th>
-            <th>ACTION</th>
-            <th>MARKER</th>
+            {headers.map((header) => {
+              return <th>{header}</th>;
+            })}
+            <th>Acción</th>
           </tr>
         </thead>
         <tbody>
-          {tableData.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {row.map((cell, cellIndex) => (
-                <td key={cellIndex}>
-                  <input type="text" value={cell} readOnly />
-                </td>
-              ))}
+          {addPoint && (
+            <tr>
+              {headers.map((header, index) => {
+                return <td key={index}><input type="text" /></td>;
+              })}
+              <td>
+                <button type="button">Guardar</button>
+              </td>
             </tr>
-          ))}
+          )}
+          {tableData.map((point, index) => {
+            return (
+              <tr key={index}>
+                {headers.map((header, index) => {
+                  return <td key={index}>{point[header]}</td>;
+                })}
+                <td>
+                  <button type="button" onClick={() => handleDelete(index)}>
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
+      <button type="button" onClick={handleAddPoint}>
+        Agregar
+      </button>
     </div>
   );
 };
